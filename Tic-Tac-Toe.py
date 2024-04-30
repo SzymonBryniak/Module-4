a8db349af2704e4613c30231ad57425138f8fe7f
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-04-30 22:02:08 +0100

    I plan to update the value of an element in the board_positions list with the value entered by the user.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,57 +1,58 @@
-# def display_board(board):
-#     print('')
+def display_board(board):
+    print(board)
+    return
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
 
 
-# def enter_move(board):
+def enter_move(board):
+    ask_for_input = int(input('enter your move '))
+
+    for pos in board:
+        print(pos, end='\n')
+
     # The function accepts the board's current status, asks the user about their move,
     # checks the input, and updates the board according to the user's decision.
 
-
+    return print(ask_for_input)
 # def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares;
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
 
-
 # def victory_for(board, sign):
     # The function analyzes the board's status in order to check if
     # the player using 'O's or 'X's has won the game
-
 
 # def draw_move(board):
     # The function draws the computer's move and updates the board.
 
 
 plus = '+'
-dash = '-------'
+dash = '+-------'
 pipe = '|'
+a, b, c, d, e, f, g, h, i = 1, 2, 3, 4, 5, 6, 7, 8, 9
+board_positions = [[[a], [b], [c]],
+                   [[d], [e], [f]],
+                   [[g], [h], [i]]]
 
-bs = [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]]
-a = 'p'
-board = f'''
+board1 = f'''
 +-------+-------+-------+
 |       |       |       |
-|  {a}    |   {a}   |   {a}   |
-|       |       |       |
-+-------+-------+-------+
-|       |       |       |
-|  {a}    |   {a}   |   {a}   |
+|   {a}   |   {b}   |   {c}   |
 |       |       |       |
 +-------+-------+-------+
 |       |       |       |
-|  {a}    |   {a}   |   {a}   |
+|   {d}   |   {e}   |   {f}   |
+|       |       |       |
++-------+-------+-------+
+|       |       |       |
+|   {g}   |   {h}   |   {i}   |
 |       |       |       |
 +-------+-------+-------+
 '''
-board1 = ['+', '-------', '+', '-------', '+', '-------', '+']
-board2 = [[dash for char in range(0, 1)] for char1 in range(0, 3)]
-
-board1_join = ''.join(board1)
+# board1 = ['+', '-------', '+', '-------', '+', '-------', '+']
+# board2 = [dash for char in range(0, 1)]
 
 
-#board2_join = (''.join(board2[0]))
-
-print(board1_join)
-print(len(board2))
-print(board)
+display_board(board1)
+enter_move(board_positions)
