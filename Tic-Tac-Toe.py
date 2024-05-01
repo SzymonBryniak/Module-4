8c6e963580738a8d7d5ec62a17dc71330f1a1839
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-05-01 21:30:17 +0100

    I am trying to replace an element in the list when user input is equal to the value of the element.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -8,13 +8,13 @@ def display_board(board):
 def enter_move(board):
     ask_for_input = int(input('enter your move '))
 
-    for pos in board:
-        print(pos, end='\n')
+    # for it in range(0, 9):
+    #     for pos in board
 
     # The function accepts the board's current status, asks the user about their move,
     # checks the input, and updates the board according to the user's decision.
 
-    return print(ask_for_input)
+    return print(ask_for_input in board[0][0])
 # def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares;
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
@@ -30,10 +30,10 @@ def enter_move(board):
 plus = '+'
 dash = '+-------'
 pipe = '|'
-a, b, c, d, e, f, g, h, i = 1, 2, 3, 4, 5, 6, 7, 8, 9
-board_positions = [[[a], [b], [c]],
-                   [[d], [e], [f]],
-                   [[g], [h], [i]]]
+a, b, c, d, e, f, g, h, i = '1', '2', 3, 4, 5, 6, 7, 8, 9
+board_positions = [[[1], ['2'], [3]],
+                   [[4], [5], [6]],
+                   [[7], [8], [9]]]
 
 board1 = f'''
 +-------+-------+-------+
@@ -52,6 +52,7 @@ board1 = f'''
 '''
 # board1 = ['+', '-------', '+', '-------', '+', '-------', '+']
 # board2 = [dash for char in range(0, 1)]
+
 
 
 display_board(board1)
