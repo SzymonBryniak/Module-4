c15f63b6e09989eb09ff6f347a9bc5048b780e94
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-05-05 18:40:53 +0100

    Successful board update

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,34 +1,32 @@
 def display_board(board):
-    print(board)
-    abc_counter = -1
-    a, b, c, d, e, f, g, h, i = 1, 2, 3, 4, 5, 6, 7, 8, 9
-    abc = [a, b, c, d, e, f, g, h, i]
+    abc_counter = 0
+    abc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9}
+    abcd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    abc2 = {}
     for ii in range(0, 3):
         for kk in range(0, 3):
+            abc2.update({abcd[abc_counter]: board[ii][kk][0]})
             abc_counter += 1
-            abc[abc_counter] = board[ii][kk]
-            print(abc_counter, board[ii][kk])
-
-    abc_counter += 1
+    print(abc2, board[-1])
 
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
     board1 = f'''
-    +-------+-------+-------+
-    |       |       |       |
-    |   {a}   |   {b}   |   {c}   |
-    |       |       |       |
-    +-------+-------+-------+
-    |       |       |       |
-    |   {d}   |   {e}   |   {f}   |
-    |       |       |       |
-    +-------+-------+-------+
-    |       |       |       |
-    |   {g}   |   {h}   |   {i}   |
-    |       |       |       |
-    +-------+-------+-------+
++-------+-------+-------+
+|       |       |       |
+|   {abc2['a']}   |   {abc2['b']}   |   {abc2['c']}   |
+|       |       |       |
++-------+-------+-------+
+|       |       |       |
+|   {abc2['d']}   |   {abc2['e']}   |   {abc2['f']}   |
+|       |       |       |
++-------+-------+-------+
+|       |       |       |
+|   {abc2['g']}   |   {abc2['h']}   |   {abc2['i']}   |
+|       |       |       |
++-------+-------+-------+
     '''
-    return print(board1), print(abc, abc_counter)
+    return print(board1)
 
 
 def enter_move(board):
@@ -39,10 +37,11 @@ def enter_move(board):
             if ask_for_input in board[bb][cc]:
                 #print('yes')
                 board[bb][cc] = 'O'
+                board.append(ask_for_input)
 
     # The function accepts the board's current status, asks the user about their move,
     # checks the input, and updates the board according to the user's decision.
-
+    #board.append(ask_for_input)
     return display_board(board)
 # def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares;
